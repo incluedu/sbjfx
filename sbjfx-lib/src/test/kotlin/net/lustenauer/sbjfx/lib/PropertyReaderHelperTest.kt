3a293174e2d5f46033928fbe90d57e8aac27a71a
Patric Hollenstein <patric.hollenstein@lustenauer.net>
2023-08-27 18:13:43 +0200

    SBFX-39: Use mockk instead mockito in all test classes -> update PropertyReaderHelperTest

--- sbjfx-lib/src/test/kotlin/net/lustenauer/sbjfx/lib/PropertyReaderHelperTest.kt
+++ sbjfx-lib/src/test/kotlin/net/lustenauer/sbjfx/lib/PropertyReaderHelperTest.kt
@@ -1,41 +1,37 @@
 package net.lustenauer.sbjfx.lib
 
+import io.mockk.every
+import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.mockito.ArgumentMatchers
-import org.mockito.Mockito
 import org.springframework.core.env.Environment
 
 internal class PropertyReaderHelperTest {
 
     private lateinit var envArrayMock: Environment
+
     private lateinit var envSingleEntryMock: Environment
 
     @BeforeEach
     fun setUp() {
-        envArrayMock = Mockito.mock(Environment::class.java)
-        envSingleEntryMock = Mockito.mock(Environment::class.java)
+        envArrayMock = mockk<Environment>()
+        envSingleEntryMock = mockk<Environment>()
         // This is what Spring environment returns
-        // When we defined an array in of appicons: ('- entry_1' ... in the
-        // yaml):
-        Mockito.`when`(envArrayMock.getProperty("entry")).thenReturn(null)
-        Mockito.`when`(envArrayMock.getProperty("entry[0]")).thenReturn("entry_0")
-        Mockito.`when`(envArrayMock.getProperty("entry[1]")).thenReturn("entry_1")
-        Mockito.`when`(envArrayMock.getProperty("entry[2]")).thenReturn("entry_2")
+        // When we defined an array in of appicons: ('- entry_1' ... in the yaml):
+        every { envArrayMock.getProperty(any()) } returns null
+        every { envArrayMock.getProperty("entry[0]") } returns "entry_0"
+        every { envArrayMock.getProperty("entry[1]") } returns "entry_1"
+        every { envArrayMock.getProperty("entry[2]") } returns "entry_2"
 
         // When there is a single entry:
-        Mockito.`when`(envSingleEntryMock.getProperty("entry")).thenReturn("entry")
-        Mockito.`when`(envSingleEntryMock.getProperty("entry[0]")).thenReturn(null)
-        Mockito.`when`(
-            envSingleEntryMock.getProperty(
-                ArgumentMatchers.eq("stringentry"), ArgumentMatchers.eq(
-                    String::class.java
-                )
-            )
-        ).thenReturn("entry")
+        every { envSingleEntryMock.getProperty(any()) } returns null
+        every { envSingleEntryMock.getProperty("entry") } returns "entry"
+
+        every { envSingleEntryMock.getProperty(any(), String::class.java) } returns null
+        every { envSingleEntryMock.getProperty("stringentry", String::class.java) } returns "entry"
     }
 
     @Test
@@ -73,9 +69,7 @@ internal class PropertyReaderHelperTest {
             env = envSingleEntryMock,
             key = "no_entry",
             type = String::class.java
-        ) { theEntryValue: String ->
-            testObject.stringEntry = theEntryValue
-        }
+        ) { theEntryValue: String -> testObject.stringEntry = theEntryValue }
         assertThat(testObject.stringEntry).isEqualTo("UNSET")
     }
 
